f00902a1e04707b14313e8286870a7f520807ec0
Remy Blok <remy.blok@prodware.nl>
2022-04-29 12:57:32 +0200

    fix theme switcher

--- assets/script/theme-toggle.js
+++ assets/script/theme-toggle.js
@@ -1,21 +1,19 @@
-let darkMode = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
 const html = document.querySelector('html');
+let darkMode = localStorage.getItem("darkMode") ? JSON.parse(localStorage.getItem("darkMode"))
+         : (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
 const toggleTheme = function() {
-    darkMode = !darkMode
-    let theme = darkMode ? "theme-dark" : "theme-light";
-    localStorage.setItem("theme", theme);
-    
-    html.classList.remove(html.classList.toString() || "theme-default");
-    html.classList.add(theme);
+    darkMode = !darkMode;
+    localStorage.setItem("darkMode", darkMode.toString());
+    setTheme();
 }
 
-var storedTheme = localStorage.getItem("theme");
-if (storedTheme)
-{
-    html.classList.add(storedTheme);
-    darkMode = storedTheme === "theme-dark";
+const setTheme = function() {
+    html.classList.add(darkMode ? "theme-dark" : "theme-light");
+    html.classList.remove(!darkMode ? "theme-dark" : "theme-light");
 }
 
+setTheme();
 const toggleButton = document.getElementById("theme-toggle")
 toggleButton.addEventListener('click', toggleTheme);
 toggleButton.checked = darkMode;
